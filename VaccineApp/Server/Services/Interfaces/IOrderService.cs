8653db7afac83e4a0a250e23d2d3ac650edcaa5e
Niko Muukkonen <niko@muukkonen.info>
2021-08-21 22:56:25 +0300

    Created services and interfaces for orders.

---
+++ VaccineApp/Server/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Server.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<List<Order>> GetOrders();
+        Task<Order> GetOrder(string id);
+    }
+}
---
+++ VaccineApp/Server/Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccineApp.Server.Data;
+using VaccineApp.Server.Services.Interfaces;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Server.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly DataContext _context;
+        public OrderService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<Order> GetOrder(string id)
+        {
+            return await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+    }
+}
