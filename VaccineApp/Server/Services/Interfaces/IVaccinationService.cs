e1b8abc29f73ecaa718c427fff7b6442625208df
Niko Muukkonen <niko@muukkonen.info>
2021-08-21 23:17:00 +0300

    Created Vaccination service and interface.

---
+++ VaccineApp/Server/Services/Interfaces/IVaccinationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Server.Services.Interfaces
+{
+    public interface IVaccinationService
+    {
+        Task<List<Vaccination>> GetVaccinations();
+        Task<Vaccination> GetVaccination(string id);
+    }
+}
---
+++ VaccineApp/Server/Services/VaccinationService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccineApp.Server.Data;
+using VaccineApp.Server.Services.Interfaces;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Server.Services
+{
+    public class VaccinationService : IVaccinationService
+    {
+        private readonly DataContext _context;
+        public VaccinationService(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<Vaccination> GetVaccination(string id)
+        {
+            return await _context.Vaccinations.FirstOrDefaultAsync(v => v.Vaccination_id == id);
+        }
+
+        public async Task<List<Vaccination>> GetVaccinations()
+        {
+            return await _context.Vaccinations.ToListAsync();
+        }
+    }
+}
