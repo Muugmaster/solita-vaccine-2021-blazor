d74080d68badeae307decf89cd94aa4dea1ac1da
Niko Muukkonen <niko@muukkonen.info>
2021-08-21 21:27:02 +0300

    Create Models

---
+++ VaccineApp/Shared/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccineApp.Shared.Models
+{
+    public class Order
+    {
+        [Key]
+        [MaxLength(200)]
+        public string Id { get; set; }
+        public int OrderNumber { get; set; }
+        [MaxLength(100)]
+        public string ResponsiblePerson { get; set; }
+        [MaxLength(20)]
+        public string HealthCareDistrict { get; set; }
+        [MaxLength(50)]
+        public string Vaccine { get; set; }
+        public int Injections { get; set; }
+        public DateTime Arrived { get; set; }
+        public List<Vaccination> Vaccinations { get; set; }
+    }
+}
---
+++ VaccineApp/Shared/Models/Vaccination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaccineApp.Shared.Models
+{
+    public class Vaccination
+    {
+        [Key]
+        [MaxLength(200)]
+        public string Vaccination_id { get; set; }
+        [MaxLength(200)]
+        public string SourceBottle { get; set; }
+        [MaxLength(20)]
+        public string Gender { get; set; }
+        public DateTime VaccinationDate { get; set; }
+
+        [ForeignKey("SourceBottle")]
+        public Order Order { get; set; }
+    }
+}
