1b8d19ebe472bc83f3c7b73f192d876df4f67b41
Niko Muukkonen <niko@muukkonen.info>
2021-08-21 21:36:54 +0300

    Create DataContext and Seed class to seed initial data to database.

---
+++ VaccineApp/Server/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccineApp.Shared.Models;
+using VaccineApp.Shared.SeedDatabase;
+
+namespace VaccineApp.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {            
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Vaccination> Vaccinations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Seed data = new Seed();
+
+            modelBuilder.Entity<Order>().HasData(data.SeedOrderData());
+            modelBuilder.Entity<Vaccination>().HasData(data.SeedVaccinationData());
+
+        }
+    }
+}
---
+++ VaccineApp/Shared/SeedDatabase/Seed.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Shared.SeedDatabase
+{
+    public class Seed
+    {
+        public List<Vaccination> SeedVaccinationData()
+        {
+            var vaccinations = new List<Vaccination>();
+            using (StreamReader r = new StreamReader(@"data\vaccinations.json"))
+            {
+                string json = r.ReadToEnd();
+                vaccinations = JsonConvert.DeserializeObject<List<Vaccination>>(json);
+            }
+            return vaccinations;
+        }
+
+        public List<Order> SeedOrderData()
+        {
+            string[] orderFiles = new string[] { "data\\zerpfy.json", "data\\solarBuddhica.json", "data\\antiqua.json" };
+            var orders = new List<Order>();
+            var temp = new List<Order>();
+            foreach (var file in orderFiles)
+            {
+                using (StreamReader r = new StreamReader(file))
+                {
+                    string json = r.ReadToEnd();
+                    temp = JsonConvert.DeserializeObject<List<Order>>(json);
+                }
+                orders.AddRange(temp);
+            }
+
+            return orders;
+        }
+    }
+}
