4be3796df7ca238dfd1f8df7565cbc420451e8b3
Niko Muukkonen <niko@muukkonen.info>
2021-08-22 12:16:08 +0300

    Create DTOs

---
+++ VaccineApp/Server/Dtos/OrderDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Server.Dtos
+{
+    public class OrderDto
+    {
+        [MaxLength(200)]
+        public string Id { get; set; }
+        public int OrderNumber { get; set; }
+        [MaxLength(100)]
+        public string ResponsiblePerson { get; set; }
+        [MaxLength(20)]
+        public string HealthCareDistrict { get; set; }
+        [MaxLength(50)]
+        public string Vaccine { get; set; }
+        public int Injections { get; set; }
+        public DateTime Arrived { get; set; }
+        public List<Vaccination> Vaccinations { get; set; }
+    }
+}
---
+++ VaccineApp/Server/Dtos/VaccinationDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using VaccineApp.Shared.Models;
+
+namespace VaccineApp.Server.Dtos
+{
+    public class VaccinationDto
+    {
+        [MaxLength(200)]
+        public string Vaccination_id { get; set; }
+        [MaxLength(200)]
+        public string SourceBottle { get; set; }
+        [MaxLength(20)]
+        public string Gender { get; set; }
+        public DateTime VaccinationDate { get; set; }
+
+        [JsonIgnore]
+        public Order Order { get; set; }
+    }
+}
